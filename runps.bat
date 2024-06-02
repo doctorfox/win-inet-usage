7b3bf5c3f2cdb13f9e268d4ee3ecb2975d4ab962
Doctor Fox <drfox29@yahoo.co.uk>
2024-06-02 18:22:11 +0100

    Update runps.bat

--- runps.bat
+++ runps.bat
@@ -1,5 +1,1 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "F:\src\winbat\netstats.ps1"
-
-#Ignore the lines below as they were used in testing
-#PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""F:\src\winbat\netstats.ps1""'}"
-#pause
+PowerShell -NoProfile -ExecutionPolicy Bypass -File ".\netstats.ps1"
